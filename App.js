c454e8376b6648507d887e1c76ad80aeef550249
rajyasrinidhimedikonda <rajyasrinidhimedikonda@gmail.com>
2023-04-01 23:40:00 -0500

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1f1d2d',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+const img = document.createElement("img");
+img.src = "/Users/rajyasrinidhimedikonda/Desktop/my stuff/pHinder/images/Background_Element.png";
+document.body.appendChild(img);
+
+
